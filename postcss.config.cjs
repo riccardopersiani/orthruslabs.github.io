ea81c20aeab57bc46e0033754121065afc7ca809
Riccardo Persiani <r.persiani92@gmail.com>
2022-01-02 16:03:45 +0100

    update tailwind

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -4,7 +4,7 @@ const autoprefixer = require("autoprefixer");
 const config = {
   plugins: [
     //Some plugins, like tailwindcss/nesting, need to run before Tailwind,
-    tailwindcss(),
+    require('tailwindcss')('./tailwind.config.js'),
     //But others, like autoprefixer, need to run after,
     autoprefixer,
   ],
